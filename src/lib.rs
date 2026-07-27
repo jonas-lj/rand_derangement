508c801195b44a396b692ec5076efc0c9a7e4a66
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2026-07-27 17:37:33 +0200

    Tweak is_involution doc wording

--- src/lib.rs
+++ src/lib.rs
@@ -238,7 +238,7 @@ impl Permutation {
         self.0.iter().enumerate().all(|(i, &pi)| i != pi)
     }
 
-    /// Returns `true` iff this permutation is an involution: its own inverse
+    /// Returns `true` iff this permutation is an involution, its own inverse
     /// (`self[self[i]] == i` for all `i`, equivalently every cycle has length ≤ 2).
     pub fn is_involution(&self) -> bool {
         self.0.iter().enumerate().all(|(i, &pi)| self.0[pi] == i)
